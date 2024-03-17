da1b6e4cd74a331f353db53e913a169543ecd357
Norras <arkusreturn@gmail.com>
2024-03-17 20:23:38 +0100

    Changement de la page root (s'adapte si connecté)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,39 @@
-"use client";
+"use client"
+
 import React from 'react';
 import './style.css';
+import { createClient } from '@/utils/supabase/client'
+import { redirect } from 'next/navigation'
 
-export default function Connexion(){
+export default function Home(){
+    const [style , setStyle] = React.useState({
+        color: 'black'
+    })
 
-    function login() {
-        var username = (document.getElementById('username') as HTMLInputElement).value;
-        var password = (document.getElementById('password') as HTMLInputElement).value;
-        var message = (document.getElementById('loginMessage') as HTMLParagraphElement);
-    
-        
-    
-        // Pour cet exemple, nous affichons simplement un message.
-        if (username === 'utilisateur' && password === 'motdepasse') {
-            message.style.color = 'green';
-            message.textContent = 'Connexion réussie!';
-        } else {
-            message.style.color = 'red';
-            message.textContent = 'Nom d\'utilisateur ou mot de passe incorrect.';
+    const checksession = async () => {
+        const supabase = createClient()
+        const { data, error } = await supabase.auth.getUser()
+        if (error || !data?.user) {
+            setStyle({color: 'red'});
+            const home_div = document.getElementById('home') as HTMLDivElement;
+            if ( !document.getElementById('home_a') ) {
+                var home_a = document.createElement('a');
+                home_a.id = 'home_a';
+                home_a.textContent = 'Connectez-vous';
+                home_a.href = '/signin';
+                home_div.appendChild(home_a);
+            }
         }
-    
     }
-    function redirectToInscription() {
-        window.location.href = '/inscription';
-    }
+
+    React.useEffect(() => {
+        checksession()
+    }, [])
+
     return (
-        <div className="container">
-            <h1>Connexion</h1>
-            <form id="loginForm">
-                <label htmlFor="username">Nom d'utilisateur:</label>
-                <input type="text" id="username" name="username" required/>
-                <label htmlFor="password">Mot de passe:</label>
-                <input type="password" id="password" name="password" required/>
-
-                <button type="button" onClick={login}>Se connecter</button>
-            </form>
-            <p id="loginMessage"></p>
-
-            <div className="links">
-                <a href="#">Mot de passe oublié ?</a>
-                <span>|</span>
-                <a href="#" onClick={redirectToInscription}>S'inscrire</a>
-            </div>
+        <div id="home">
+            <h1>Home</h1>
+            <p style={style}>Bienvenue sur notre site!</p>
         </div>
     )
 }
