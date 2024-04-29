6aed6dffc8b25e256a7c1d7f0ae68ddb8b4c8964
Norras <arkusreturn@gmail.com>
2024-04-29 15:10:53 +0200

    Page de Livre devenue dynamique

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -6,24 +6,21 @@ import Navbar from '@/components/ui/header';
 import Footer from '@/components/ui/footer';
 
 // Définir une interface pour les données du livre
-interface Livre {
+interface Book {
   id: number;
+  date: string;
+  summary: string;
+  ISBN: string;
+  genre: string;
   title: string;
-  author: string;
-  summary: string; // Résumé du livre
-  image: string; // URL de l'image du livre
+  author: string; // Nouvel attribut pour le nom de l'auteur
+  available: boolean;
+  image: string;
 }
 
 const LivrePage = () => {
   // Supposons que nous avons un livre fictif
-  const [livre, setLivre] = useState<Livre>({
-    id: 1,
-    title: "Technologies et protocoles Internet",
-    author: "Douglas Adams",
-    summary: "Un résumé du livre...",
-    image: "/chemin/vers/image.jpg",
-    // D'autres informations sur le livre...
-  });
+  const [livre, setLivre] = useState<Book>();
 
   // Fonction pour gérer la réservation du livre et enregistrer la date
   const handleReservation = () => {
@@ -33,15 +30,38 @@ const LivrePage = () => {
     // Par exemple, faire une requête à un backend pour enregistrer la réservation avec la date
   };
 
+  const fetchBook = async () => {
+    // get book id from the URL
+    const bookId = window.location.pathname.split('/').pop();
+    console.log(bookId);
+    // fetch the book data from the API
+    const request = new Request('http://localhost:3000/api/books/id/' + bookId,{
+      method: 'GET'
+    });
+    const response = await fetch(request);
+    const data = await response.json();
+    console.log(data);
+    if (data.result) {
+      setLivre(data.result);
+    } else {
+      alert('Le livre demandé n\'existe pas');
+    }
+  }
+
+  React.useEffect(() => {
+    fetchBook();
+  }, []);
+
+
   return (
     <div className="bg-blanc-100 min-h-screen">
       <Navbar />
       <div className="container mx-auto py-8">
     
         {/* Affichage des informations du livre */}
-        <h1 className="text-3xl font-bold mb-4 text-center">{livre.title}</h1>
-        <p className="text-gray-700 mb-2"><span className="font-bold">Auteur</span> : {livre.author}</p>
-        <p className="text-gray-700 mb-4"><span className="font-bold">Résumé</span> : {livre.summary}</p>
+        <h1 className="text-3xl font-bold mb-4 text-center">{livre?.title}</h1>
+        <p className="text-gray-700 mb-2"><span className="font-bold">Auteur</span> : {livre?.author}</p>
+        <p className="text-gray-700 mb-4"><span className="font-bold">Résumé</span> : {livre?.summary}</p>
         {/* Autres informations sur le livre */}
 
         {/* Bouton pour réserver le livre */}
