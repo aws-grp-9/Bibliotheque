93618c4c11e7a6e2e762455e881bd0665a17a2f1
Norras <arkusreturn@gmail.com>
2024-04-29 15:15:57 +0200

    petit changement pour le résumé

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -40,9 +40,16 @@ const LivrePage = () => {
     });
     const response = await fetch(request);
     const data = await response.json();
-    console.log(data);
     if (data.result) {
       setLivre(data.result);
+      if (livre?.summary !== ""){
+        const div = document.getElementById('info_div');
+        // add child 
+        const p = document.createElement('p');
+        p.className = "text-gray-700 mb-4";
+        p.innerHTML = `<span class="font-bold">Résumé</span> : ${livre?.summary}`;
+        div?.appendChild(p);
+      }
     } else {
       alert('Le livre demandé n\'existe pas');
     }
@@ -56,12 +63,11 @@ const LivrePage = () => {
   return (
     <div className="bg-blanc-100 min-h-screen">
       <Navbar />
-      <div className="container mx-auto py-8">
+      <div className="container mx-auto py-8" id='info_div'>
     
         {/* Affichage des informations du livre */}
         <h1 className="text-3xl font-bold mb-4 text-center">{livre?.title}</h1>
         <p className="text-gray-700 mb-2"><span className="font-bold">Auteur</span> : {livre?.author}</p>
-        <p className="text-gray-700 mb-4"><span className="font-bold">Résumé</span> : {livre?.summary}</p>
         {/* Autres informations sur le livre */}
 
         {/* Bouton pour réserver le livre */}
