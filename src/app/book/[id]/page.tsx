f2c43533f955a95d639fa5b942af37394082844b
Norras <arkusreturn@gmail.com>
2024-05-02 16:12:21 +0200

    Description on bookpage fixed

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -12,7 +12,7 @@ import { useParams } from 'next/navigation';
 interface Book {
   id: number;
   date: string;
-  summary: string;
+  description: string;
   ISBN: string;
   genre: string;
   title: string;
@@ -44,14 +44,24 @@ const LivrePage = () => {
     });
     const response = await fetch(request);
     const data = await response.json();
+    console.log(data);
     if (data.result) {
       setLivre(data.result);
-      if (livre?.summary !== "" && livre?.summary !== undefined){
+      console.log(data?.result.title);
+      if (data?.result.description !== "" && data?.result.description !== undefined){
         const div = document.getElementById('info_div');
-        const p = document.createElement('p');
-        p.className = "text-gray-700 mb-4";
-        p.innerHTML = `<span class="font-bold">Résumé</span> : ${livre?.summary}`;
-        div?.appendChild(p);
+        let p = div?.querySelector('.book-description');
+        if (!p) {
+          p = document.createElement('p');
+          p.className = "text-gray-700 mb-4 book-description max-w-96";
+          let description = data?.result.description;
+          if (description.length > 255) {
+            description = description.substring(0, 255) + '...';
+          }
+          p.innerHTML = `<span class="font-bold">Résumé</span> : ${description}`;
+          div?.insertBefore(p, div.querySelector('button'));
+          console.log("Inserted");
+        }
       }
     } else {
       alert('Le livre demandé n\'existe pas');
@@ -68,19 +78,29 @@ const LivrePage = () => {
   return (
     <div className="bg-blanc-100 min-h-screen">
       <Navbar />
-      <div className="container mx-auto py-8" id='info_div'>
-    
+      <div className="container mx-auto py-8">
+        
         {/* Affichage des informations du livre */}
         <h1 className="text-3xl font-bold mb-4 text-center">{livre?.title}</h1>
-        <img src={livre?.image} alt="Image du livre" className="mb-4 w-40" />
-        <p className="text-gray-700 mb-2"><span className="font-bold">Auteur</span> : {livre?.author}</p>
-        {/* Autres informations sur le livre */}
+        <div id='book_div' className='flex justify-around'>
+          <div id='info_div'>
+          <img src={livre?.image} alt="Image du livre" className="mb-4 w-40" />
+          <p className="text-gray-700 mb-2"><span className="font-bold">Auteur</span> : {livre?.author}</p>
+          {/* Autres informations sur le livre */}
 
-        {/* Bouton pour réserver le livre */}
-        <button className="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4 transition-transform duration-300 hover:scale-110" onClick={() => setShowModal(!showModal)}>
-          Réserver ce livre
-        </button>
-        {showModal && <AvailabilitiesModal setShowModal={setShowModal} bookId={bookId.toString()} />}
+          {/* Bouton pour réserver le livre */}
+          <button className="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4 transition-transform duration-300 hover:scale-110" onClick={() => setShowModal(!showModal)}>
+            Réserver ce livre
+          </button>
+        </div>
+        <div id='reviews_div'>
+          {/* Affichage des avis sur le livre */}
+          <h2 className="text-2xl font-bold mb-4">Avis</h2>
+          <p className="text-gray-700 mb-4">Aucun avis pour le moment</p>
+
+        </div> 
+      </div> 
+      {showModal && <AvailabilitiesModal setShowModal={setShowModal} bookId={bookId.toString()} />}
       </div>
       <Footer />
     </div>
