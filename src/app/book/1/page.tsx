3f7cd1e0e7e1e54eef1d88d278f786577e05d288
uvsq22307000 <sabine.challal@yahoo.com>
2024-05-03 23:44:59 +0200

    modification de  la page de reservations des livres

--- src/app/book/1/page.tsx
+++ src/app/book/1/page.tsx
@@ -23,7 +23,7 @@ const LivrePage = () => {
     title: "Le Guide du Voyageur Galactique",
     author: "Douglas Adams",
     summary: "Un résumé du livre...",
-    image: " /informatique.jpg",
+    image: " /technologie et protocole internet.jpeg",
     available: true, // Livre initialement disponible
   });
 
@@ -41,37 +41,40 @@ const LivrePage = () => {
 
   return (
     <div className="bg-blanc-100 min-h-screen py-10 px-4">
-      <Navbar />
-      <div className="container mx-auto mt-10 flex justify-center items-center">
-        <div className="max-w-3/4 flex justify-between items-center">
-          {/* Section du contenu textuel */}
-          <div className="w-1/2">
-            <h1 className="text-3xl font-bold mb-6 text-center">{livre.title}</h1>
-            <p className="font-bold mb-4">Auteur: {livre.author}</p>
-            {/* Condition pour afficher le bouton de réservation ou le message */}
-            {livre.available ? (
-              <button 
-                className="bg-blue-500 text-white py-2 px-4 rounded-md transition duration-300 hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
-                onClick={handleReservation}
-              >
-                Réserver ce livre
-              </button>
-            ) : (
-              <p className="text-red-500 font-bold mb-4">Ce livre a été réservé</p>
-            )}
-          </div>
-          {/* Section de l'image du livre */}
-          <div className="w-1/3 relative">
-            <img 
-              src={livre.image} 
-              alt={livre.title} 
-              className="max-w-1/3 h-auto rounded-full hover:scale-50 transition-transform duration-50 cursor-pointer" 
-            />
-          </div>
+    <Navbar />
+    <div className="container mx-auto mt-10 flex justify-center items-center">
+      <div className="max-w-3/4 flex justify-between items-center">
+        {/* Section de l'auteur et du bouton de réservation */}
+        <div className="w-1/3 text-left pr-2 pb-2">
+          <p className="font-bold mb-4">Auteur: {livre.author}</p>
+          {/* Condition pour afficher le bouton de réservation ou le message */}
+          {livre.available ? (
+            <button 
+              className="bg-blue-500 text-white py-2 px-4 rounded-md transition duration-300 hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
+              onClick={handleReservation}
+            >
+              Réserver ce livre
+            </button>
+          ) : (
+            <p className="text-red-500 font-bold mb-4">Ce livre a été réservé</p>
+          )}
+        </div>
+        {/* Section du titre du livre */}
+        <div className="w-1/2 space-y-4">
+          <h1 className="text-3xl font-bold mb-6 text-center text-rose-poudree">{livre.title}</h1>
+        </div>
+        {/* Section de l'image du livre */}
+        <div className="w-1/3 relative text-right">
+          <img 
+            src={livre.image} 
+            alt={livre.title} 
+            className="max-w-1/3 h-auto rounded-full hover:scale-50 transition-transform duration-50 cursor-pointer" 
+          />
         </div>
       </div>
-      <Footer />
     </div>
+    <Footer />
+  </div>
   );
 };
 
